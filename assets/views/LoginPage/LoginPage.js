66d2f7685c10be7b3caa26536254cb7be72957e6
Cory Siebler <cory.siebler@er2.com>
2021-04-02 14:52:10 -0700

    Initial JSS to SASS conversion for the login page

--- assets/views/LoginPage/LoginPage.js
+++ assets/views/LoginPage/LoginPage.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { useOktaAuth } from "@okta/okta-react";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import PageLayout from "components/PageLayout/PageLayout";
 import LoginFormSection from "./Sections/LoginFormSection";
 
-const useStyles = makeStyles((theme) => ({
-  content: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(2, 0, 1),
-  },
-}));
-
 const LoginPage = () => {
   const { authState } = useOktaAuth();
-  const classes = useStyles();
 
   if (authState.isPending) {
     return <div>Loading...</div>;
