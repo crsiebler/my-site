eab26dd5caefabeb3d0cea54680d0e53779c20bb
Cory Siebler <cory.siebler@protonmail.com>
2021-04-03 20:40:23 -0700

    Initial conversion of JSS to SASS for Landing page. (#54)
    Resolves #48

--- assets/views/LandingPage/LandingPage.js
+++ assets/views/LandingPage/LandingPage.js
@@ -1,29 +1,19 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 import PageLayout from "components/PageLayout/PageLayout";
 import LandingHeroSection from "./Sections/LandingHeroSection";
 import LandingSkillsSection from "./Sections/LandingSkillsSection";
+import "styles/modules/_m-landing.scss";
 
-const useStyles = makeStyles((theme) => ({
-  content: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(2, 0, 1),
-  },
-}));
-
-const LandingPage = () => {
-  const classes = useStyles();
-  return (
-    <PageLayout title="Home">
-      <Container maxWidth={false} className={classes.content}>
-        <LandingHeroSection />
-        <Divider />
-        <LandingSkillsSection />
-      </Container>
-    </PageLayout>
-  );
-};
+const LandingPage = () => (
+  <PageLayout title="Home">
+    <Container maxWidth={false} className="m-landing">
+      <LandingHeroSection />
+      <Divider />
+      <LandingSkillsSection />
+    </Container>
+  </PageLayout>
+);
 
 export default LandingPage;
--- assets/views/LandingPage/Sections/LandingSkillsGridSection.js
+++ assets/views/LandingPage/Sections/LandingSkillsGridSection.js
@@ -3,20 +3,14 @@ import Grid from "@material-ui/core/Grid";
 import SkillCard from "components/SkillCard/SkillCard";
 import { skills } from "constants/skillConstants";
 
-const LandingSkillsGridSection = () => {
-  return (
-    <Grid container alignItems="stretch" justify="center" spacing={1}>
-      {skills.map((skill, index) => (
-        <Grid key={index} item xs={6} sm={4} md={3}>
-          <SkillCard
-            title={skill.title}
-            icons={skill.icons}
-            body={skill.body}
-          />
-        </Grid>
-      ))}
-    </Grid>
-  );
-};
+const LandingSkillsGridSection = () => (
+  <Grid container alignItems="stretch" justify="center" spacing={1}>
+    {skills.map((skill, index) => (
+      <Grid key={index} item xs={6} sm={4} md={3}>
+        <SkillCard title={skill.title} icons={skill.icons} body={skill.body} />
+      </Grid>
+    ))}
+  </Grid>
+);
 
 export default LandingSkillsGridSection;
